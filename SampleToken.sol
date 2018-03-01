58557a154dc5244d7864d4b3983b60bcb66ca530
pilsogood <good.seungpil.choi@gmail.com>
2018-03-01 21:53:46 +0900

    Sample Token Source

---
+++ SampleToken.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.18;
+
+contract SampleToken {
+
+ 	/* 전체 배열 밸런스 설정 */
+	mapping (address => uint256) public balanceOf;
+
+    /* 토큰 계약 초기화 */
+    function SampleToken() public {
+      timeOfLastHalving = now;
+    }
+
+    
+
+}
