08ba1c2b647326348f7ae202d529bbc511233918
pilsogood <good.seungpil.choi@gmail.com>
2018-03-01 21:55:26 +0900

    Edited

--- SampleToken.sol
+++ SampleToken.sol
@@ -1,15 +1,13 @@
 pragma solidity ^0.4.18;
 
 contract SampleToken {
-
- 	/* 전체 배열 밸런스 설정 */
+	
+	/* 전체 배열 밸런스 설정 */
 	mapping (address => uint256) public balanceOf;
 
-    /* 토큰 계약 초기화 */
+	/* 토큰 계약 초기화 */
     function SampleToken() public {
       timeOfLastHalving = now;
     }
-
     
-
 }
