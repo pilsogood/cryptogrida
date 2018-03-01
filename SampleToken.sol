7f1522d70c85ac490d4ec59ef80fd201c9b30893
pilsogood <good.seungpil.choi@gmail.com>
2018-03-01 22:38:11 +0900

    add transfer method

--- SampleToken.sol
+++ SampleToken.sol
@@ -6,13 +6,12 @@ contract SampleToken {
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
 
-    /* 전체 배열 밸런스 설정 */
+    /* 변수 설정 */
     string public constant _name = "SampleToken";                   // 토큰 이름
-    string public constant _symbol = "SST";	                        // 토큰 심볼
+    string public constant _symbol = "SST";                         // 토큰 심볼
     uint256 public constant _initialSupply= 42000000000000;         // 토큰 공급량 
     uint8 public constant _decimal = 18;
 
-    /* 변수 설정 */
     string public name;
     string public symbol;
     uint8 public decimals;
@@ -21,10 +20,18 @@ contract SampleToken {
     /* 토큰 계약 초기화 */
     function SampleToken( uint256 initialSupply, string tokenName, string tokenSymbol) public {
         decimals = _decimal;
-        totalSupply = _initialSupply * (10 ** uint256(_decimal));   // 총 공급량을 설정해준다
-        balanceOf[msg.sender] = totalSupply;                        // 작성자에게 공급량 전체를 할당해 준다
-        name = _name;                                               // 표시할 심볼을 설정해준다
+        totalSupply = _initialSupply * (10 ** uint256(_decimal));   // 총 공급량을 설정
+        balanceOf[msg.sender] = totalSupply;                        // sender 에게 공급량 전체를 할당
+        name = _name;                                               // 표시할 심볼을 설정
         symbol = _symbol;
+    }
+
+    /* 코인 전송 */
+    function transfer(address _to, uint256 _value) {
+        if (balanceOf[msg.sender] < _value) throw;                   // sender가 충분한 코인이 있는지 확인
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw;         // 오버플로우 체크 
+        balanceOf[msg.sender] -= _value;                             // sender에게서 전송한 코인양을 뺌
+        balanceOf[_to] += _value;                                    // 수신자에게는 전송한 코인양을 더함 
     }
 
 }
